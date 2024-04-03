ac6260cf4f90758eb292e7137ecd94fa9cb40cb3
Michael Weiser <michael.weiser@eviden.com>
2024-04-03 16:57:32 +0200

    Add OU option

--- specs/default/cluster-init/scripts/001_ad-int.sh
+++ specs/default/cluster-init/scripts/001_ad-int.sh
@@ -7,6 +7,7 @@
 AD_SERVER=$(jetpack config adauth.ad_server)
 AD_SERVER_IP=$(jetpack config adauth.ad_server_ip)
 AD_DOMAIN=$AD_SERVER
+AD_OU=$(jetpack config adauth.ad_ou)
 ADMIN_NAME=$(jetpack config adauth.ad_admin_user)
 ADMIN_PASSWORD=$(jetpack config adauth.ad_admin_password)
 
@@ -42,7 +43,7 @@ max_retry=3
 
 while true; do
 logger -s "Domain join on $AD_DOMAIN"
-echo $ADMIN_PASSWORD| realm join -U $ADMIN_NAME $AD_DOMAIN
+echo $ADMIN_PASSWORD| realm join ${AD_OU:+--computer-ou} $AD_OU -U $ADMIN_NAME $AD_DOMAIN
 
 if [ ! -f "/etc/sssd/sssd.conf" ]; then
     if [[ $n -le $max_retry ]]; then
