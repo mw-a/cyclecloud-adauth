d3d1fd020cfc2e205bb62843efee5415c00cea48
Michael Weiser <michael.weiser@eviden.com>
2024-05-27 08:38:13 +0000

    Do not configure DNS servers
    ... as it's not necessary - they'll be taken from the DHCP response anyway.
    
    Also protect ifcfg configuration from cloud-init resetting it on reboot.

--- specs/default/cluster-init/scripts/003_dns.sh
+++ specs/default/cluster-init/scripts/003_dns.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-DNS_SERVERS=$(jetpack config dns.servers)
 SEARCH_LIST=$(jetpack config dns.search_list)
 
-if [ -n "$SEARCH_LIST" -o -n "$DNS_SERVERS" ] ; then
+if [ -n "$SEARCH_LIST" ] ; then
 	restorecon /etc/sysconfig/network-scripts/ifcfg-*
-	nmcli c m "System eth0" ipv4.ignore-auto-dns yes ipv4.dns-search "$SEARCH_LIST" ipv4.dns "$DNS_SERVERS"
+	nmcli c m "System eth0" ipv4.dns-search "$SEARCH_LIST"
+	# prevent cloud-init from resetting the file on reboot
+	chattr +i /etc/sysconfig/network-scripts/ifcfg-eth0
 	nmcli c u "System eth0"
 fi
