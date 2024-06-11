6b3bfde1c837c7ae514d4fb8c258678bb7213bb4
Michael Weiser <michael.weiser@eviden.com>
2024-06-11 06:44:04 +0000

    Only enable slurm services on slurm nodes

--- specs/default/chef/site-cookbooks/ad/recipes/slurm_service_enable.rb
+++ specs/default/chef/site-cookbooks/ad/recipes/slurm_service_enable.rb
@@ -1,22 +1,24 @@
-defer_block 'Delayed enablement of services' do
-	service "munge" do
-		action :enable
-	end
-
-	role = node[:slurm][:role]
-	if role == "execute"
-		service "slurmd" do
-			action :enable
-		end
-	end
-
-	if role == "scheduler"
-		service "slurmdbd" do
+if not node[:slurm].nil?
+	defer_block 'Delayed enablement of services' do
+		service "munge" do
 			action :enable
 		end
 
-		service "slurmctld" do
-			action :enable
+		role = node[:slurm][:role]
+		if role == "execute"
+			service "slurmd" do
+				action :enable
+			end
+		end
+
+		if role == "scheduler"
+			service "slurmdbd" do
+				action :enable
+			end
+
+			service "slurmctld" do
+				action :enable
+			end
 		end
 	end
 end
