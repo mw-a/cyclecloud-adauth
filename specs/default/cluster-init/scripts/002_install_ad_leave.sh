9f0409749a9e109d44a6d9e3285768d803da1bdf
Michael Weiser <michael.weiser@eviden.com>
2024-06-17 08:38:14 +0000

    Fix AD leave for special node name handling

--- specs/default/cluster-init/scripts/002_install_ad_leave.sh
+++ specs/default/cluster-init/scripts/002_install_ad_leave.sh
@@ -10,8 +10,15 @@ ADMIN_NAME=\$(jetpack config adauth.ad_admin_user)
 ADMIN_PASSWORD=\$(jetpack config adauth.ad_admin_password)
 #SITE_DC=\$(/opt/cycle/jetpack/scripts/site_info.py -D "\$AD_DOMAIN" --eager | head -1)
 
+# work around 21 char length limit by shortening partition names - make
+# sure to produce no collisions here when adding new ones
+AD_COMPUTERNAME=\$(hostname)
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/hpc-/h}
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/gpu-/g}
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/login-/l}
+
 # -S "\$SITE_DC"
-echo "\$ADMIN_PASSWORD" | adcli delete-computer --stdin-password -U "\$ADMIN_NAME" -D "\$AD_DOMAIN" "\$HOSTNAME"
+echo "\$ADMIN_PASSWORD" | adcli delete-computer --stdin-password -U "\$ADMIN_NAME" -D "\$AD_DOMAIN" "\$AD_COMPUTERNAME"
 EOF
 cat >/opt/cycle/jetpack/scripts/onPreempt.sh << EOF
 #!/bin/sh
@@ -20,8 +27,15 @@ ADMIN_NAME=\$(jetpack config adauth.ad_admin_user)
 ADMIN_PASSWORD=\$(jetpack config adauth.ad_admin_password)
 #SITE_DC=\$(/opt/cycle/jetpack/scripts/site_info.py -D "\$AD_DOMAIN" --eager | head -1)
 
+# work around 21 char length limit by shortening partition names - make
+# sure to produce no collisions here when adding new ones
+AD_COMPUTERNAME=\$(hostname)
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/hpc-/h}
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/gpu-/g}
+AD_COMPUTERNAME=\${AD_COMPUTERNAME/login-/l}
+
 # -S "\$SITE_DC"
-echo "\$ADMIN_PASSWORD" | adcli delete-computer --stdin-password -U "\$ADMIN_NAME" -D "\$AD_DOMAIN" "\$HOSTNAME"
+echo "\$ADMIN_PASSWORD" | adcli delete-computer --stdin-password -U "\$ADMIN_NAME" -D "\$AD_DOMAIN" "\$AD_COMPUTERNAME"
 EOF
 chmod +x /opt/cycle/jetpack/scripts/onTerminate.sh
 chmod +x /opt/cycle/jetpack/scripts/onPreempt.sh
